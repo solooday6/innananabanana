99a08a28fbc070265779447f3395ebe5b051ae05
solooday6 <141855464+solooday6@users.noreply.github.com>
2023-08-10 03:31:08 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+ let nama = 'Inna al bashir'
+ let pesanUntuk = 'Putri Rasilia Ali'
+ let usia = 20
+alert('Halo, ini ' + nama + ', pesan ini untuk ' + pesanUntuk + ' tulisan biasa-biasa saja untuk sahabat saya yang sekarang sudah berusia ' + usia + ' tahun dan lagi mau KM (ciyee KaEm)' + '. Semoga pesan ini bisa memberi kebahagiaan, Aamiin ')
